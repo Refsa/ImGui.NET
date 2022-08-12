9768e3728f38ec08a2549bb38a6e9c5b21d4d3eb
Refsa <refsa.dev@gmail.com>
2022-08-12 23:50:52 +0200

    some helper scripts to run gen and nuget pack

---
+++ src/nuget_pack.sh
@@ -0,0 +1,4 @@
+dotnet pack ImGui.NET/ImGui.NET.csproj -c Release --output packed
+dotnet pack ImPlot.NET/ImPlot.NET.csproj -c Release --output packed
+dotnet pack ImGuizmo.NET/ImGuizmo.NET.csproj -c Release --output packed
+dotnet pack ImNodes.NET/ImNodes.NET.csproj -c Release --output packed
